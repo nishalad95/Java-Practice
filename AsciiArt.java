d332a1c4c8ae7aa9d33b41f457e9f80327237dc1
nishalad95 <nishalad95@gmail.com>
2016-05-03 22:41:37 -0700

    Update AsciiArt.java

--- AsciiArt.java
+++ AsciiArt.java
@@ -1,5 +1,4 @@
-/*Nisha Lad, CSE 142, Winter 2016, Section BE -Michelle Cho
-*Programming Homework Assignment #2, 01/13/16
+/*Nisha Lad
 *This program produces AsciiArt of the Empire State Building
 */
 
